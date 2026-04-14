c5d62a6e981cbb0ec67e87c1c99712e5e55b7eef
Aayurt <aayurtshrestha@gmail.com>
2026-04-15 00:37:15 +0545

    Cahgnes

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -23,7 +23,7 @@ export default function LoginPage() {
         router.push("/dashboard");
       }
     });
-  }, [router]);
+  }, []);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
